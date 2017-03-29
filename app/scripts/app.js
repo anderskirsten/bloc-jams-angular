bf3549e0c78290ac874b971a053c51c47f7af776
Kirsten Anderson <anders.kirsten@gmail.com>
2017-03-29 13:06:44 -0500

    Update ui-router to v0.4.2 + detail editing: Page Load Errors Fixed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,9 @@
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-            enabled: true,
-            requireBase: false
-        });
+                enabled: true,
+                requireBase: false
+            });
 
         $stateProvider
             .state('landing', {
@@ -18,13 +18,13 @@
             })
 
             .state('collection', {
-            url: '/collection',
-            templateUrl: '/templates/collection.html'
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
             });
     }
 
     angular
-        .module('blocJams', ['ui-router'])
+        .module('blocJams', ['ui.router'])
         .config(config);
 })();
 
