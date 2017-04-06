f945bbfc2e13c32d0fe4f8de0170bf9e03198c03
Kirsten Anderson <anders.kirsten@gmail.com>
2017-04-06 13:54:01 -0500

    Checkpoint 6 Angular: Complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,9 @@
 (function() {
-    function AlbumCtrl() {
-        this.albumData = albumPicasso;
-        this.songs = [];
-        for(i = 0; i < this.albumData.songs.length; i++){
-          this.songs.push(this.albumData.songs[i]);
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
         }
-        console.log(this.albumData.artist);
-     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
