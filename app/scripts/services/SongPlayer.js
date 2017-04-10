aca13bb5b0315d686e548d0e6b8f0e7ee91e21e5
Kirsten Anderson <anders.kirsten@gmail.com>
2017-04-10 11:09:37 -0500

    Checkpoint 8 Angular: Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,12 +1,12 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
 
         /**
-        * @desc placeholder for currently selected song
+        * @desc current album info
         * @type {Object}
         */
-        var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
 
         /**
         * @desc Buzz object audio file
@@ -22,7 +22,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioURL, {
@@ -30,7 +30,7 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         }
 
         /**
@@ -41,19 +41,35 @@
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
-        }
+        };
+
+        /**
+        * @function getSongIndex
+        * @desc provides index number of currently playing song
+        * @param {Object} song
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+
+        /**
+        * @desc **PUBLIC** active song object from list of songs
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
 
         /**
         * @method SongPlayer.play
-        * @desc **PUBLIC** Evaluates current condition of play to start the selected song _uses `setSong` & `playSong` as helpers_
+        * @desc **PUBLIC** Evaluates Scurrent condition of play to start the selected song _uses `setSong` & `playSong` as helpers_
         * @param {Object} song
         */
         SongPlayer.play = function(song) {
-             if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
 
-             } else if (currentSong === song) {
+             } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                 }
@@ -66,8 +82,31 @@
         * @param {Object} song
         */
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
+        };
+
+        /**
+        * @method SongPlayer.previous
+        * @desc **PUBLIC**
+        1. uses getSongIndex function to retrieve current song's index number, then decrements the index number by one so it matches the visible song number
+        2. evaluates if current song is at index zero - if yes, stops playing current song & sets `SongPlayer.currentSong.playing` to null
+        3. if no, sets value of `song` to the current song index from the current album, then uses `setSong` function to stop playing the currentBuzzObject & set currently playing song to null & finally uses the `playSong` function to start playing the newly selected (aka previous) song in the song list
+        * @param N/A
+        */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
 
         return SongPlayer;
@@ -75,5 +114,5 @@
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
